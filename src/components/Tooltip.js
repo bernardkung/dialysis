a523182dbaa4ca61979f21fa215c6f80c0fcd0f7
bernard kung <speak.freely@gmail.com>
2024-02-22 00:56:34 -0500

    basic tooltip component created, not tested or implemented

---
+++ src/components/Tooltip.js
@@ -0,0 +1,19 @@
+
+const Tooltip = ({ data, name, pos }) => {
+  if (!data) {
+    return null;
+  }
+  return (
+    <div
+      className={name}
+      style={{
+        left: pos.x,
+        top: pos.y, 
+      }}
+    >
+      { data.name }
+    </div>
+  )
+}
+
+export default Tooltip
